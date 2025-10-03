6e9c8ef28b5c30eab6ed6ba9f8ad3f79cd2affb8
Yosep simatupang <yosepmsimatupang678@gmail.com>
2025-10-03 09:51:13 +0700

    init

--- src/features/auth/layouts/AuthLayout.jsx
+++ src/features/auth/layouts/AuthLayout.jsx
@@ -1,15 +1,13 @@
-import { Outlet, NavLink, useLocation } from 'react-router-dom';
+import { Outlet, NavLink } from 'react-router-dom';
 
 function AuthLayout() {
-  const location = useLocation();
-
   return (
     <div className="container">
+      {/* Baris ini penting untuk membuat konten berada di tengah secara vertikal dan horizontal */}
       <div className="row justify-content-center align-items-center vh-100">
         <div className="col-md-5">
           <div className="card shadow-sm">
             <div className="card-header p-0">
-              {/* Navigasi Tab */}
               <ul className="nav nav-tabs nav-fill">
                 <li className="nav-item">
                   <NavLink className="nav-link" to="/auth/login">
@@ -24,7 +22,6 @@ function AuthLayout() {
               </ul>
             </div>
             <div className="card-body p-4">
-              {/* Outlet akan merender LoginPage atau RegisterPage */}
               <Outlet />
             </div>
           </div>
